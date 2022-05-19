42db4f18173c86ed434895acf3aef2c3468284d0
Ritesh Patel <ritpatel2278@gmail.com>
2022-05-19 18:34:58 -0500

    initial commit

---
+++ android/app/src/main/kotlin/uk/org/valvolunteering/valvolunteering/MainActivity.kt
@@ -0,0 +1,6 @@
+package uk.org.valvolunteering.valvolunteering
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'src/web_view_stack.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(home: Scaffold(body: WebViewLoad()));
+  }
+}
+
+class WebViewLoad extends StatefulWidget {
+  const WebViewLoad({Key? key}) : super(key: key);
+
+  @override
+  WebViewLoadUI createState() => WebViewLoadUI();
+}
+
+class WebViewLoadUI extends State<WebViewLoad> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        toolbarHeight: 1,
+        backgroundColor: Colors.white,
+        elevation: 0,
+      ),
+      body: const WebViewStack(),
+    );
+  }
+}
