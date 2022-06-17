8fa73429ce79868418d9f6c3db0f632ecc7b994b
Ritesh Patel <ritpatel2278@gmail.com>
2022-06-17 10:24:20 -0500

    updates to icons

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: Scaffold(body: WebViewLoad()));
+    return const MaterialApp(
+      home: Scaffold(body: WebViewLoad()),
+      debugShowCheckedModeBanner: false,
+    );
   }
 }
 
